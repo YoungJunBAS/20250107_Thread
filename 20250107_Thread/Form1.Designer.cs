1f71e3ff86b9394d94461789096a9955432a12f1
YoungJunBAS <ayjayj0218@gmail.com>
2025-01-07 13:46:51 +0900

    Add project files.

---
+++ 20250107_Thread/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace _20250107_Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(38, 52);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(712, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(38, 209);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(712, 23);
+            this.progressBar3.TabIndex = 1;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(38, 294);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(712, 23);
+            this.progressBar4.TabIndex = 2;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(38, 376);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(712, 23);
+            this.progressBar5.TabIndex = 3;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(38, 127);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(712, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox7.Location = new System.Drawing.Point(38, 16);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 25);
+            this.textBox7.TabIndex = 5;
+            this.textBox7.TabStop = false;
+            this.textBox7.Text = "NO.1 CAR";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(38, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "NO.2 CAR";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(38, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TabStop = false;
+            this.textBox3.Text = "NO.3 CAR";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(38, 250);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TabStop = false;
+            this.textBox4.Text = "NO.4 CAR";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(38, 334);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 25);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TabStop = false;
+            this.textBox5.Text = "NO.5 CAR";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(38, 405);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(712, 33);
+            this.button_start.TabIndex = 10;
+            this.button_start.Text = "Start!";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button_start;
+    }
+}
+
---
+++ 20250107_Thread/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Reflection;
+
+namespace _20250107_Thread
+{
+    public partial class Form1 : Form
+    {
+        ProgressBar[] progress_arr = new ProgressBar[5];
+        Thread[] threads = new Thread[5];
+        int[] timeTable = new int[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            progress_arr[0] = progressBar1;
+            progress_arr[1] = progressBar2;
+            progress_arr[2] = progressBar3;
+            progress_arr[3] = progressBar4;
+            progress_arr[4] = progressBar5;
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int index = i;
+                threads[index] = new Thread(() => RaceTheCar(progress_arr[index], index)); // 람다식은 매개변수를 떼우고, 원하는 메소드를 실행하게 한다.
+                threads[index].Start();
+            }
+
+
+            //Thread thread1 = new Thread(() => RaceTheCar(progressBar1));
+            //Thread thread2 = new Thread(() => RaceTheCar(progressBar2));
+            //Thread thread3 = new Thread(() => RaceTheCar(progressBar3));
+            //Thread thread4 = new Thread(() => RaceTheCar(progressBar4));
+            //Thread thread5 = new Thread(() => RaceTheCar(progressBar5));
+
+            //thread1.Start();
+            //thread2.Start();
+            //thread3.Start();
+            //thread4.Start();
+            //thread5.Start();            
+        }
+        private void RaceTheCar(ProgressBar progressBar, int index)
+        { 
+            int cnt = 0;
+            int sumTime = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                // 자동차 경주 로직 (예: 랜덤한 시간 동안 대기)
+                int randomSleep = new Random().Next(100, 1001);
+                Thread.Sleep(randomSleep);
+
+                // UI 스레드에 ProgressBar 값 업데이트 요청 (Invoke 사용)
+                if (progressBar.InvokeRequired)
+                {
+                    // Invoke를 발생시켜서 간접적으로 컨트롤을 업데이트
+                    progressBar.Invoke(new MethodInvoker(() =>
+                    {
+                        progressBar.Value = i;
+                    }));
+                }
+                sumTime += randomSleep;
+                cnt++;
+            }
+            //MessageBox.Show("No." + (index + 1) + "의 도착시간 : " + sumTime + "ms");
+            timeTable[index] = sumTime;
+        }
+    }
+}
